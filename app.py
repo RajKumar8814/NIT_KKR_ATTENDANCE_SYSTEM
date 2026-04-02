066db755e6da2243acbfb2a9ab1ced6896baab77
RajKumar8814 <rk844304@gmail.com>
2026-04-03 02:20:11 +0530

    Cleaning project for Railway deployment

--- app.py
+++ app.py
@@ -53,5 +53,7 @@ def create_app():
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
